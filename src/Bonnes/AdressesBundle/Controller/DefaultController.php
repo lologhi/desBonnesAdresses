c158f599fcd97ad5c10ef198d2492258898b770a
Laurent Ghirardotti <laurent@ghirardotti.fr>
2014-03-12 17:38:37 +0100

    First controller (not working ?)

--- src/Bonnes/AdressesBundle/Controller/DefaultController.php
+++ src/Bonnes/AdressesBundle/Controller/DefaultController.php
@@ -4,10 +4,17 @@ namespace Bonnes\AdressesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('BonnesAdressesBundle:Default:index.html.twig', array('name' => $name));
+class DefaultController extends Controller {
+    
+    public function indexAction($name) {
+        $product = $this->get('doctrine_mongodb')->getRepository('BonnesAdressesBundle:Adresse')->findAll();
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$name);
+        }
+        
+        var_dump($product);
+        
+        return $this->render('BonnesAdressesBundle:Default:index.html.twig', array('name' => $product));
     }
 }
