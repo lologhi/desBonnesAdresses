18b5b8cc1403cdecdf0cf81c0f525893a19e75e7
Laurent Ghirardotti <laurent@ghirardotti.fr>
2014-04-28 19:22:44 +0200

    Better center and ajax call for details

--- src/Bonnes/AdressesBundle/Controller/DefaultController.php
+++ src/Bonnes/AdressesBundle/Controller/DefaultController.php
@@ -3,30 +3,37 @@
 namespace Bonnes\AdressesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 class DefaultController extends Controller {
-    
+
     public function indexAction() {
         $products = $this->get('doctrine_mongodb')->getRepository('BonnesAdressesBundle:Adresse')->findAll();
 
         if (!$products) {
             throw $this->createNotFoundException('No product found');
         }
-        
+
         //var_dump($product);
-        
+
         return $this->render('BonnesAdressesBundle:Default:index.html.twig', array('products' => $products));
     }
-    
-    public function nameAction($name = 'Garance') {
-        $products = $this->get('doctrine_mongodb')->getRepository('BonnesAdressesBundle:Adresse')->findOneByName($name);
 
-        if (!$products) {
-            throw $this->createNotFoundException('No product found');
+    public function addressAction($id) {
+        $address = $this->get('doctrine_mongodb')->getRepository('BonnesAdressesBundle:Adresse')->find($id);
+
+        if (!$address) {
+            throw $this->createNotFoundException('No address found');
         }
-        
-        //var_dump($product);
-        
-        return $this->render('BonnesAdressesBundle:Default:index.html.twig', array('products' => $products));
+
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new GetSetMethodNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonAddress = $serializer->serialize($address, 'json');
+
+        return new Response($jsonAddress);
     }
 }
