ec61f76a66fe78b03eacea4bb183d3816794779d
Laurent Ghirardotti <laurent@ghirardotti.fr>
2014-05-05 11:39:21 +0200

    Décomposition du twig et config Assetic

---
+++ src/Bonnes/AdressesBundle/Resources/public/js/mymap.js
@@ -0,0 +1,107 @@
+$(document).foundation();
+
+//http://stackoverflow.com/questions/9895951/how-to-display-multiple-markers-with-individual-framecloud-popups-in-openlayers
+
+var map, mappingLayer, bingRoad, vectorLayer, selectMarkerControl, selectedFeature;
+OpenLayers.ImgPath = "/bundles/bonnesadresses/js/img/"
+
+function onFeatureSelect(feature) {
+	selectedFeature = feature;
+	popup = new OpenLayers.Popup.FramedCloud("tempId", feature.geometry.getBounds().getCenterLonLat(), null, selectedFeature.attributes.Name, null, true);
+	feature.popup = popup;
+	map.addPopup(popup);
+	$.post(
+		"{{ path('bonnes_adresses_namepage') }}",
+		{idAddress: feature.data.Id},
+		function(response){
+			//console.log(response);
+			if(response.id == feature.data.Id) {
+				$('#content tbody').empty();
+				$.each(response, function( index, value ) {
+					if(value == null) {
+					} else {
+						$('#content tbody').append( '<tr><td>' + index + '</td><td>' + value + '</tr>' );
+					}
+				});
+			}
+		},
+		"json");
+	//document.title = response.name;
+	if(selectedFeature.attributes.Url != window.location) {
+		// L'URL actuelle n'est pas la même que la théorique
+		window.history.pushState(selectedFeature.attributes.Url, selectedFeature.attributes.Name, selectedFeature.attributes.Url);
+	}
+	//console.log(feature.data.Id);
+}
+
+function onFeatureUnselect(feature) {
+	map.removePopup(feature.popup);
+	feature.popup.destroy();
+	feature.popup = null;
+}
+
+function init(){
+	map = new OpenLayers.Map( 'map');
+	var apiKey = 'Am9j4sJYRl62rzLT5ux_MRqxxkvw4fzCaH8KS1kdAcjvg_QDznR_DhaTlZ7qhq3C';
+
+	// Default OSM imagerySet
+	mappingLayer = new OpenLayers.Layer.OSM("OpenStreetMap");
+	// Bing's Road imagerySet (http://openlayers.org/blog/2010/12/18/bing-tiles-for-openlayers/)
+	// Et https://www.bingmapsportal.com/
+	bingRoad = new OpenLayers.Layer.Bing({key: apiKey, type: "Road"});
+	map.addLayers([bingRoad, mappingLayer]);
+	map.addControl(new OpenLayers.Control.LayerSwitcher());
+
+	vectorLayer = new OpenLayers.Layer.Vector("Vector Layer", {projection: "EPSG:4326"});
+
+	selectMarkerControl = new OpenLayers.Control.SelectFeature(vectorLayer, {onSelect: onFeatureSelect, onUnselect: onFeatureUnselect});
+	map.addControl(selectMarkerControl);
+
+	var s = new OpenLayers.Style({'pointRadius': 10, 'externalGraphic': '/bundles/bonnesadresses/js/img/marker.png'});
+	new OpenLayers.StyleMap(s);
+
+	selectMarkerControl.activate();
+	map.addLayer(vectorLayer);
+
+	var scaleline = new OpenLayers.Control.ScaleLine();
+	map.addControl(scaleline);
+
+	center();
+}
+
+// Centré sur Notre Dame de Paris
+function center() {
+	map.setCenter(new OpenLayers.LonLat(2.3499021, 48.8529682).transform("EPSG:4326", map.getProjectionObject()), 13);
+}
+
+function placeMyMarkers(lat, lon, name, url, id) {
+	var lonLat = new OpenLayers.Geometry.Point( lon, lat);
+	lonLat.transform("EPSG:4326", map.getProjectionObject());
+	var style = OpenLayers.Util.extend({
+		fillColor : "#0A00FF",
+		graphicOpacity: 1
+	}, OpenLayers.Feature.Vector.style['default']);
+	var randomFeature = new OpenLayers.Feature.Vector(lonLat, { Name: name, Url: url, Id: id, Lon : lon, Lat : lat}, style);
+	vectorLayer.addFeatures(randomFeature);
+}
+
+function placeMySpecificMarkers(lat, lon, name, url, id) {
+	var lonLat = new OpenLayers.Geometry.Point( lon, lat);
+	lonLat.transform("EPSG:4326", map.getProjectionObject());
+	var specialStyle = OpenLayers.Util.extend({
+		externalGraphic : "/bundles/bonnesadresses/js/img/marker-blue.png",
+		graphicOpacity: 1
+	}, OpenLayers.Feature.Vector.style['default']);
+	var randomFeature = new OpenLayers.Feature.Vector(lonLat, { Name: name, Url: url, Id: id, Lon : lon, Lat : lat}, specialStyle);
+	vectorLayer.addFeatures(randomFeature);
+}
+
+init();
+
+/* http://www.tinywall.info/2012/02/change-browser-url-without-page-reload-refresh-with-ajax-request-using-javascript-html5-history-api-php-jquery-like-facebook-github-navigation-menu.html */
+/* the below code is to override back button to get the ajax content without page reload*/
+/*$(window).bind('popstate', function() {
+	$.ajax({url:location.pathname+'?rel=tab',success: function(data){
+		$('#content').html(data);
+	}});
+});/**/
