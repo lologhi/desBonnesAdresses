a3b20d395644958d5344bad57cbc4b537bb152b9
Laurent Ghirardotti <laurent@ghirardotti.fr>
2014-05-19 15:51:26 +0200

    Add Admin for AddresseType collection

---
+++ src/Bonnes/AdressesBundle/Admin/AdresseTypeAdmin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bonnes\AdressesBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class AdresseTypeAdmin extends Admin {
+
+	// Fields to be shown on create/edit forms
+	protected function configureFormFields(FormMapper $formMapper) {
+
+		$formMapper->with('Général')
+			->add('name')
+		;
+	}
+
+	// Fields to be shown on filter forms
+	protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+		$datagridMapper
+			->add('name')
+		;
+	}
+
+	// Fields to be shown on lists
+	protected function configureListFields(ListMapper $listMapper) {
+		$listMapper
+			->addIdentifier('name')
+		;
+	}
+}
