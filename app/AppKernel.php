7cede8396f0d3b004a31eedf5734c01bffb9f401
Laurent Ghirardotti <laurent@ghirardotti.fr>
2014-12-22 22:43:51 +0100

    It was the wrong bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Presta\SitemapBundle\PrestaSitemapBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
