8d3d660884dcdf47f1366482232f897b30c5b68e
Laurent Ghirardotti <laurent@ghirardotti.fr>
2014-12-28 21:59:54 +0100

    Fix admin with new Sonata

--- src/Bonnes/AdressesBundle/Admin/AdresseAdmin.php
+++ src/Bonnes/AdressesBundle/Admin/AdresseAdmin.php
@@ -11,28 +11,25 @@ class AdresseAdmin extends Admin {
 
     // Fields to be shown on create/edit forms
     protected function configureFormFields(FormMapper $formMapper) {
-        $formMapper
-            ->with('Général', array('class' => 'col-md-6'))
-            ->with('Blabla', array('class' => 'col-md-6'))
-        ;
-
-        $formMapper->with('Général')
+        $formMapper->with('Général', array('class' => 'col-md-6'))
             ->add('name')
             ->add('adresse')
             ->add('codePostal')
             ->add('ville')
             ->add('telephone')
             ->add('url')
+			->end()
         ;
 
-        $formMapper->with('Blabla')
+        $formMapper->with('Blabla', array('class' => 'col-md-6'))
             ->add('latitude')
             ->add('longitude')
             ->add('marker')
-            ->add('type', null, array('required' => false, 'by_reference' => false, 'multiple' => false))
+            ->add('type', null, array('required' => false, 'by_reference' => false))
             ->add('origine', null, array('required' => false))
             ->add('description', 'textarea', array('required' => false))
             ->add('prix', null, array('required' => false))
+			->end()
         ;
     }
 
